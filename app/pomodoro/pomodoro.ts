a971ce0acaef9b51fa9dda6ac387db972569e0ba
romorin <romorin@zoho.com>
2016-11-10 18:16:49 -0500

    use pomodoro-ts-core submodule; typo in index.html

--- app/pomodoro/pomodoro.ts
+++
@@ -1,9 +0,0 @@
-export interface Pomodoro  {
-	onEdit(): void;
-	onToggle(): void;
-	onReset(): void;
-	incrementMin(): void;
-	decrementMin(): void;
-	incrementSec(): void;
-	decrementSec(): void;
-}
